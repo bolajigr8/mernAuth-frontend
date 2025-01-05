60f82f5ab5aedde3b32a114d5220d2a1fb90ad18
Bolaji Adebayo <badebayo11@gmail.com>
2025-01-05 20:27:59 +0100

    new Debug

--- middleware.ts
+++ middleware.ts
@@ -15,35 +15,14 @@ export default async function middleware(req: NextRequest) {
   const isProtectedRoute = protectedRoutes.includes(path)
   const isPublicRoute = publicRoutes.includes(path)
 
-  // Retrieve accessToken from cookies
-  let accessToken =
-    req.cookies.get('accessToken')?.value ||
-    req.headers
-      .get('cookie')
-      ?.split('; ')
-      .find((cookie) => cookie.startsWith('accessToken='))
-      ?.split('=')[1]
+  const accessToken = req.cookies.get('accessToken')?.value
 
-  // Handle routes requiring authentication
   if (isProtectedRoute && !accessToken) {
     return NextResponse.redirect(new URL('/signup', req.nextUrl))
   }
 
-  // Handle public routes when the user is already authenticated
   if (isPublicRoute && accessToken) {
     return NextResponse.redirect(new URL('/home', req.nextUrl))
-  }
-
-  // If no token is found, set a default token for testing (avoid infinite loop)
-  if (!accessToken) {
-    const response = NextResponse.next()
-    response.cookies.set('accessToken', 'testAccessToken', {
-      httpOnly: true,
-      secure: true,
-      path: '/',
-      sameSite: 'lax', // Ensure the cookie is handled properly
-    })
-    return response
   }
 
   return NextResponse.next()
