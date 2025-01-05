5f36d51541df55caf748a43a8c3d0f027bd09ef8
Bolaji Adebayo <badebayo11@gmail.com>
2025-01-05 01:48:46 +0100

    new cimmit

--- middleware.ts
+++ middleware.ts
@@ -5,8 +5,8 @@ const publicRoutes = [
   '/',
   '/signup',
   '/confirm-account',
-  '/forgot-password',
-  '/reset-password',
+  'forgot-password',
+  'reset-password',
   '/verify-mfa',
 ]
 
@@ -14,25 +14,16 @@ export default async function middleware(req: NextRequest) {
   const path = req.nextUrl.pathname
   const isProtectedRoute = protectedRoutes.includes(path)
   const isPublicRoute = publicRoutes.includes(path)
+
   const accessToken = req.cookies.get('accessToken')?.value
 
-  if (isProtectedRoute) {
-    if (!accessToken) {
-      console.log(`Redirecting to login: Path = ${path}`)
-
-      return NextResponse.redirect(new URL('/signup', req.nextUrl))
-    }
+  if (isProtectedRoute && !accessToken) {
+    return NextResponse.redirect(new URL('/signup', req.nextUrl))
   }
 
   if (isPublicRoute && accessToken) {
-    console.log(`Redirecting to home: Path = ${path}`)
-    // if (path !== '/home') {
-    //   // Prevent redirecting to '/home' when already on it
-    //   return NextResponse.redirect(new URL('/home', req.nextUrl))
-    // }
+    return NextResponse.redirect(new URL('/home', req.nextUrl))
   }
 
-  // Allow all other routes to proceed
-  console.log(`Allowing access: Path = ${path}`)
   return NextResponse.next()
 }
